7a358fbb7cc5b6784a5af857bab51cd39092c9a9
tracer-ics2019 <tracer@njuics.org>
2019-09-13 15:36:50 +0800

    >  compile 181250012 piper Linux piper 5.0.0-27-generic #28-Ubuntu SMP Tue Aug 20 19:53:07 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:36:50 up  4:28,  1 user,  load average: 0.19, 0.37, 0.39 333899256e7f9ffa9d9096d6c402da0a6b9e1f92

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -39,7 +39,8 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args){
-  cpu_exec(atoi(args));
+  if(!strlen(args)) {cpu_exec(1);}
+  else {cpu_exec(atoi(args));}
   return 0;
 }
 
