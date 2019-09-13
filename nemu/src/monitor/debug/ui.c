e4a1b1a7b5f5d3873f070f378d97b21c2e440b5c
tracer-ics2019 <tracer@njuics.org>
2019-09-13 13:58:31 +0800

    >  compile 181250012 piper Linux piper 5.0.0-27-generic #28-Ubuntu SMP Tue Aug 20 19:53:07 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:58:31 up  2:49,  1 user,  load average: 0.25, 0.36, 0.32 e6579e466b5d372639ebecb6f23bf478dbeedf5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,11 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args){
+  cpu_exec(args[1]);
+  return 0;
+}
+
 static struct {
   char *name;
   char *description;
@@ -46,7 +51,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si [N]", "Executes n instructions,default n=0"},  
+  { "si [N]", "Executes n instructions,default n=0", cmd_si},  
   /* TODO: Add more commands */
 
 };
