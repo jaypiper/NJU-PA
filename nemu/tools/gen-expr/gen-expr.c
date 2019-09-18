05a3f0a3cdfa1f8883360ad3d82e8910fe31df81
tracer-ics2019 <tracer@njuics.org>
2019-09-19 07:36:24 +0800

    >  compile 181250012 piper Linux piper 5.0.0-27-generic #28-Ubuntu SMP Tue Aug 20 19:53:07 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  07:36:24 up 51 min,  1 user,  load average: 0.39, 0.46, 0.39 d2791054bb566e9715fc40bbc83802265fbf078c

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -4,11 +4,41 @@
 #include <time.h>
 #include <assert.h>
 #include <string.h>
-
 // this should be enough
+#define ran(x) (srand(time(NULL)) % x)
+#define rand_op() (srand(time(NULL)) % 4)
 static char buf[65536];
+static int buf_position = 0;
+static uint32_t gen_rand_num(){
+  return srand(time(NULL));
+}
+static char gen_rand_op(){
+  int op_rand = rand_op();
+  switch(op_rand){
+    case 0: return '+'; break;
+    case 1: return '-'; break;
+    case 2: return '*'; break;
+    default: return '/'; break;
+  }
+}
 static inline void gen_rand_expr() {
   buf[0] = '\0';
+  switch(ran(3)){
+    case 0: uint32_t a = gen_rand_num();
+            uint32_t b = a, bit_num = 0;
+            while(b != 0){ b /= 10; bit_num ++; }
+            for(int i = bit_num - 1; i >= 0; i --){ 
+              buf[buf_position + i] = (a % 10) + '0';
+              a /= 10;
+              buf_position ++;
+            }
+            break;
+    case 1: buf[buf_position] = '(';   buf_position ++;
+            gen_rand_expr();
+            buf[buf_position] = '(';   buf_position ++
+            break;
+    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr();
+  }
 }
 
 static char code_buf[65536];
