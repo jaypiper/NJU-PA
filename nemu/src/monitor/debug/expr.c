8b07bf013d4f26a1594e017aaba83b91ae651f25
tracer-ics2019 <tracer@njuics.org>
2019-09-18 20:57:33 +0800

    >  compile 181250012 piper Linux piper 5.0.0-27-generic #28-Ubuntu SMP Tue Aug 20 19:53:07 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:57:33 up  3:58,  1 user,  load average: 0.60, 0.52, 0.54 90d9162c7599d51a7d0ad3a212a2f52d707cfea

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -149,6 +149,7 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
+  printf("nr_token: %d\n",nr_token);
   printf("answer is %d\n",eval(0,nr_token-1));
   /* TODO: Insert codes to evaluate the expression. */
   
