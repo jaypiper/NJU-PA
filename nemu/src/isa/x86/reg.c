d6a78d62c3228ba254041eb0cb610946c48c6c1c
tracer-ics2019 <tracer@njuics.org>
2019-09-25 08:57:20 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:57:20 up 17 min,  1 user,  load average: 0.70, 0.82, 0.58 9930aa959d8fc42df856e0bd398b43bc96243c31

--- nemu/src/isa/x86/reg.c
+++ nemu/src/isa/x86/reg.c
@@ -53,7 +53,7 @@ uint32_t isa_reg_str2val(const char *s, bool *success) {
   uint32_t num = 0;
   bool find_reg = false;
   for(int i = 0;i < 8;i ++){
-    printf("%s\n",regsl[i]);
+    printf("%s:: %s\n",regsl[i],s);
     if(s == regsl[i]){ find_reg = true ; num = cpu.gpr[i]._32;break;}
     else if(s == regsw[i]){ find_reg = true ; num = cpu.gpr[i]._16; break;}
     else if(s == regsb[i]) { find_reg = true ; num = cpu.gpr[i]._8[i/4]; break;}
