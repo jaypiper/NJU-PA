82c1a074a08769553354709eb97307f1bffd559e
tracer-ics2019 <tracer@njuics.org>
2019-10-04 11:09:01 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  11:09:01 up  3:04,  1 user,  load average: 0.79, 0.72, 0.62 3d920573c0a2969c916ac916357bd02656da205f

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -14,7 +14,7 @@ make_EHelper(and) {
 }
 
 make_EHelper(xor) {
-  //id_dest -> val = c_xor(id_src -> val, id_dest -> val);
+  id_dest -> val = c_xor(id_src -> val, id_dest -> val);
   print_asm_template2(xor);
 }
 
