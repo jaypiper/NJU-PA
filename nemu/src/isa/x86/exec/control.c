b30c6251854d35f5d8abf991541b409cd04c2e50
tracer-ics2019 <tracer@njuics.org>
2019-10-04 14:55:20 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:55:20 up  6:50,  1 user,  load average: 1.10, 0.88, 0.73 d8bdb3b8db5f6951fe597c59f53a578142846f

--- nemu/src/isa/x86/exec/control.c
+++ nemu/src/isa/x86/exec/control.c
@@ -27,6 +27,7 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
   //TODO();
+  printf("goal:%x\n",decinfo.jmp_pc);
   rtl_push(pc);
   rtl_j(decinfo.jmp_pc);
   
