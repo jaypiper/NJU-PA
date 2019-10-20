de3e5429071e54211e6063abb5bfd99b80e95ca7
tracer-ics2019 <tracer@njuics.org>
2019-10-20 20:05:34 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:05:34 up  2:28,  1 user,  load average: 0.11, 0.27, 0.43 f134e57689a528590279273f951a0148ff5ea32

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -35,7 +35,7 @@ static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
   *dest = vaddr_read(cpu.esp,4);
-  cpu.esp -= 4;
+  cpu.esp += 4;
 }
 
 static inline void rtl_is_sub_overflow(rtlreg_t* dest,
