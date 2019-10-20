d55316d5274e33690d7b1d522e04f0f48ed74a6d
tracer-ics2019 <tracer@njuics.org>
2019-10-20 18:11:54 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:11:54 up 34 min,  1 user,  load average: 0.30, 0.56, 0.44 108bf0cdbf137ff3f25403ef6cbf243666be32b7

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -8,29 +8,29 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  rtl_sub(&s1,&id_src -> val,&id_dest->val);
-  operand_write(id_dest,&s1);
-  rtl_update_ZFSF(&s1,id_dest->width);
-  t0 = (ir > 0 && s0 < 0 && s1 > 0) || (ir < 0 && s0 > 0 && s1 < 0);
-  rtl_set_CF(&t0);
-  t1 = (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width)-1)))
-       && (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width) -1))) == 0 ? 0 : 1;
-  rtl_set_OF(&t1);
+  // rtl_sub(&s1,&id_src -> val,&id_dest->val);
+  // operand_write(id_dest,&s1);
+  // rtl_update_ZFSF(&s1,id_dest->width);
+  // t0 = (ir > 0 && s0 < 0 && s1 > 0) || (ir < 0 && s0 > 0 && s1 < 0);
+  // rtl_set_CF(&t0);
+  // t1 = (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width)-1)))
+  //      && (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width) -1))) == 0 ? 0 : 1;
+  // rtl_set_OF(&t1);
  
-  print_asm_template2(sub);
+  // print_asm_template2(sub);
 }
 
 make_EHelper(cmp) {
 
-  rtl_sub(&s1,&id_src -> val,&id_dest->val);
-  operand_write(id_dest,&s1);
-  rtl_update_ZFSF(&s1,id_dest->width);
-  t0 = (ir > 0 && s0 < 0 && s1 > 0) || (ir < 0 && s0 > 0 && s1 < 0);
-  rtl_set_CF(&t0);
-  t1 = (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width)-1)))
-       && (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width) -1))) == 0 ? 0 : 1;
-  rtl_set_OF(&t1);
-  print_asm_template2(cmp);
+  // rtl_sub(&s1,&id_src -> val,&id_dest->val);
+  // operand_write(id_dest,&s1);
+  // rtl_update_ZFSF(&s1,id_dest->width);
+  // t0 = (ir > 0 && s0 < 0 && s1 > 0) || (ir < 0 && s0 > 0 && s1 < 0);
+  // rtl_set_CF(&t0);
+  // t1 = (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width)-1)))
+  //      && (((id_src -> val) ^ (id_dest -> val)) & (1 << ((8*id_src -> width) -1))) == 0 ? 0 : 1;
+  // rtl_set_OF(&t1);
+  // print_asm_template2(cmp);
 }
 
 make_EHelper(inc) {
