febcd7cc322078bd18cf85979c62492895dea76c
tracer-ics2019 <tracer@njuics.org>
2019-10-20 18:15:40 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:15:40 up 38 min,  1 user,  load average: 0.73, 0.63, 0.49 55b2e5a9591020bf8d4e1f6f979316565829ff39

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -9,7 +9,7 @@ make_EHelper(add) {
 
 make_EHelper(sub) {
   rtl_sub(&s1,&id_src -> val,&id_dest->val);
- // operand_write(id_dest,&s1);
+  operand_write(id_dest,&s1);
   rtl_update_ZFSF(&s1,id_dest->width);
   t0 = (ir > 0 && s0 < 0 && s1 > 0) || (ir < 0 && s0 > 0 && s1 < 0);
   rtl_set_CF(&t0);
@@ -23,7 +23,7 @@ make_EHelper(sub) {
 make_EHelper(cmp) {
 
   rtl_sub(&s1,&id_src -> val,&id_dest->val);
-  //operand_write(id_dest,&s1);
+  operand_write(id_dest,&s1);
   rtl_update_ZFSF(&s1,id_dest->width);
   t0 = (ir > 0 && s0 < 0 && s1 > 0) || (ir < 0 && s0 > 0 && s1 < 0);
   rtl_set_CF(&t0);
