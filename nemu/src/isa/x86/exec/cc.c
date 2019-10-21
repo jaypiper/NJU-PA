e4bbd1c04e25ef482be02dc78dd2f93389d742cb
tracer-ics2019 <tracer@njuics.org>
2019-10-21 17:54:47 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:54:47 up 57 min,  1 user,  load average: 0.33, 0.39, 0.29 d97635cd1f8d96a6cd14b31c4e235728a4f25438

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -21,7 +21,6 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_S: *dest = cpu.SF; break;
     case CC_L: *dest = cpu.SF ^ cpu.OF; break;
     case CC_LE: *dest = cpu.ZF | (cpu.SF ^ cpu.OF); break;
-      TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
