6e223b3740c613145268cfefe298d1acf479693a
tracer-ics2019 <tracer@njuics.org>
2019-10-21 17:58:04 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:58:04 up  1:01,  1 user,  load average: 0.49, 0.42, 0.31 1aac21d25b009087e33d7c93894f2d9b7877bb06

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -83,7 +83,7 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  cpu.SF = ((*result) & (1 << (8*(width-1)))) == 0;
+  cpu.SF = ((*result) & (1 << (8*width-1))) ;
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
