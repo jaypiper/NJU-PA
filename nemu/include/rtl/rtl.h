0e1ca68f90b0c79ab0a799715561e45904b3e655
tracer-ics2019 <tracer@njuics.org>
2019-10-23 20:20:04 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:20:04 up  7:32,  1 user,  load average: 0.86, 0.71, 0.62 92197bc59473d8dc4cd254d51d6fd00862078333

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -139,8 +139,8 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   printf("width: %u, src: %x\n",width,*src1);
   switch(width){
-    // case 1: *dest = (rtlreg_t)(int32_t)(int8_t)*src1; break;
-    // case 2: *dest = (rtlreg_t)(int32_t)(int16_t)*src1; break;
+    case 1: *dest = (rtlreg_t)(int32_t)(int8_t)*src1; break;
+    case 2: *dest = (rtlreg_t)(int32_t)(int16_t)*src1; break;
     case 4: *dest = *src1; break;
     default: assert(0);
   }
