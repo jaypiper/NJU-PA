b9f7a86c31df2d4e0d4d8efa67bde4fbf380b3b0
tracer-ics2019 <tracer@njuics.org>
2019-10-23 18:01:50 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:01:50 up  5:14,  1 user,  load average: 0.43, 0.43, 0.43 ac85d29b325f80e41ce2c2c0dd9ed2aafc05d039

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -36,12 +36,12 @@ make_group(gp3,
 
 /* 0xfe */
 make_group(gp4,
-    EX(inc), EMPTY, EMPTY, EMPTY,
+    EX(inc), EX(dec), EMPTY, EMPTY,
     EMPTY, EMPTY, EMPTY, EMPTY)
 
 /* 0xff */
 make_group(gp5,
-    EX(inc), EMPTY, EX(call_rm), EMPTY,
+    EX(inc), EX(dec), EX(call_rm), EMPTY,
     EX(jmp_rm), EMPTY, EX(push), EMPTY)
 
 /* 0x0f 0x01*/
