c84adf68dd7aa50efed86d14967f7e96333cbe7b
tracer-ics2019 <tracer@njuics.org>
2019-10-23 18:41:03 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:41:03 up  5:53,  1 user,  load average: 0.87, 0.70, 0.54 c881feb87770edbb8abd02ab98d1f17a16000db

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -137,7 +137,8 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  if((*src1>>(8*width-1))&1) *dest |= 0xffffffff << (8*width);
+  else *dest &= 0xffffffffu >> (8*(4-width));
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
