1532e505e1fdac22c325735f142b76cc3da81ba6
tracer-ics2019 <tracer@njuics.org>
2019-10-23 19:59:35 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  19:59:35 up  7:11,  1 user,  load average: 0.77, 0.58, 0.49 567628f258e11c90473607c0bcd5f298869f5436

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -138,11 +138,12 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   switch(width){
-    case 1: *dest = (rtlreg_t)((int32_t)(int8_t)*src1); break;
-    case 2: *dest = (rtlreg_t)((int32_t)(int16_t)*src1); break;
+    case 1: *dest = (rtlreg_t)(int32_t)(int8_t)*src1; break;
+    case 2: *dest = (rtlreg_t)(int32_t)(int16_t)*src1; break;
     case 4: *dest = *src1; break;
     default: assert(0);
   }
+  printf("width: %u, dest: %u",width,*dest);
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
