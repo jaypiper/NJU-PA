83a3c4a3ebfe0569686196ce86d5af1e8e22ffd9
tracer-ics2019 <tracer@njuics.org>
2019-10-24 13:38:04 +0800

    >  run 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:38:04 up 5 min,  1 user,  load average: 0.74, 0.60, 0.28 d15762227cedfa68c5e21c7f59fd25c8eb6a53ac

--- nexus-am/tests/cputest/tests/string.c
+++ nexus-am/tests/cputest/tests/string.c
@@ -1,5 +1,4 @@
 #include "trap.h"
-#include<stdio.h>
 
 char *s[] = {
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 
@@ -12,30 +11,16 @@ char *s[] = {
 
 char str1[] = "Hello";
 char str[20];
-int strcmp_new(const char* src, const char* dst)
-{
-    int ret = 0;
-    while( !(ret = *(unsigned char*)src - *(unsigned char*)dst) && *dst)
-    {
-        src ++;
-        dst ++;
-    }
-    if( ret < 0) ret = -1;
-    else if(ret > 0) ret = 1;
-    return ret;
-}
-
 
 int main() {
-	nemu_assert(strcmp_new(s[0], s[2]) == 0);
-	nemu_assert(strcmp_new(s[0], s[1]) == -1);
-	nemu_assert(strcmp_new(s[0] + 1, s[1] + 1) == -1);
-	nemu_assert(strcmp_new(s[0] + 2, s[1] + 2) == -1);
-	nemu_assert(strcmp_new(s[0] + 3, s[1] + 3) == -1);
+	nemu_assert(strcmp(s[0], s[2]) == 0);
+	nemu_assert(strcmp(s[0], s[1]) == -1);
+	nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
+	nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
+	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
 
+	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 
-//	nemu_assert(strcmp_new( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
-       
 	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
 	return 0;
