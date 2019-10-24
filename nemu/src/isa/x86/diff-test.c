dd5aa6fa49c49af0afe07ff92074ae2fcba35057
tracer-ics2019 <tracer@njuics.org>
2019-10-24 22:38:48 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:38:48 up  1:29,  1 user,  load average: 0.38, 0.77, 0.92 cb900e3c29fa25dd192d846abdac37d8782f07

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,6 +2,9 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  uint32_t arr[77];
+  ref_difftest_getregs(arr);
+  for(int i = 0; i < 8; i++) printf("%d: %x  %x\n",i,arr[i],cpu.gpr[i]._32);
   // for(int i = 0;i < 8; i++) printf("%d: %x  %x \n",i,ref_r->gpr[i]._32,cpu.gpr[i]._32);
   // printf("pc: %x  %x\n",ref_r->pc,pc);
   return true;
