f1dcc3938bffbc96fd283052943a4625713dcb5e
tracer-ics2019 <tracer@njuics.org>
2019-10-24 22:33:32 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:33:32 up  1:24,  1 user,  load average: 0.74, 0.93, 1.00 15213007670abbe3f6bbf4d997db01b5c6d33dca

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -115,8 +115,8 @@ void difftest_step(vaddr_t ori_pc, vaddr_t next_pc) {
     return;
   }
 
-  ref_difftest_exec(1);
   ref_difftest_getregs(&ref_r);
+  ref_difftest_exec(1);
 for(int i = 0;i < 8; i++) printf("%d: %x  %x \n",i,ref_r.gpr[i]._32,cpu.gpr[i]._32);
   printf("pc: %x  %x\n",ref_r.pc,ori_pc);
   checkregs(&ref_r, ori_pc);
