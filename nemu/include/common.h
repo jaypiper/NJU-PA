aded51e59121ab6fa07666be6dd4b00153c98647
tracer-ics2019 <tracer@njuics.org>
2019-10-24 20:30:39 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:30:39 up  4:30,  1 user,  load average: 0.67, 0.66, 0.62 bad6bf8c2b678d57249a430081e2f2e4e8c35933

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -2,7 +2,7 @@
 #define __COMMON_H__
 
 #define DEBUG
-//#define DIFF_TEST
+#define DIFF_TEST
 
 #if _SHARE
 // do not enable these features while building a reference design
@@ -28,6 +28,7 @@ typedef uint16_t ioaddr_t;
 
 #define false 0
 #define true 1
+
 
 #include "debug.h"
 #include "macro.h"
--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,11 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  for(int i = 0; i < 8; i++){
+    if(!(ref_r->gpr[i]._32==cpu.gpr[i]._32)) return false;
+  }
+  
+  return ref_r->pc == pc;
 }
 
 void isa_difftest_attach(void) {
