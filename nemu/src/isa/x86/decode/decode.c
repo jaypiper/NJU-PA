467436008b76ffa06c81379dc8c0034126b6b975
tracer-ics2019 <tracer@njuics.org>
2019-10-24 13:56:12 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:56:12 up 23 min,  1 user,  load average: 0.62, 0.74, 0.56 ebc3b3ebae6984dad5a787d6f943f894a7e1645

--- nemu/src/isa/x86/decode/decode.c
+++ nemu/src/isa/x86/decode/decode.c
@@ -261,10 +261,11 @@ make_DHelper(a2O) {
 }
 
 make_DHelper(J) {
-  printf("esp:  %x",cpu.esp);
+  printf("esp:  %x\n",cpu.esp);
   decode_op_SI(pc, id_dest, false);
   // the target address can be computed in the decode stage
   decinfo.jmp_pc = id_dest->simm + *pc;
+    printf("esp:  %x\n",cpu.esp);
 
 }
 
