29f82eb2803ebbd83fa910845616d65f37bd3e85
tracer-ics2019 <tracer@njuics.org>
2019-10-27 10:15:08 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  10:15:08 up  1:55,  1 user,  load average: 0.70, 0.81, 0.78 5dacec8fdb17062bc6bb82a88d0b90f5dd7ae3bd

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -145,7 +145,7 @@ static OpcodeEntry opcode_table [512] = {
   /* 0x5c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x64 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x68 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x68 */	EMPTY, IDEX(I_E2G,imul3), EMPTY, EMPTY,
   /* 0x6c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x70 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x74 */	EMPTY, EMPTY, EMPTY, EMPTY,
