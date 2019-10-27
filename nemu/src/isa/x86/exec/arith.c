bf0fa4530befaaded6c59b95035af1abf3c707a2
tracer-ics2019 <tracer@njuics.org>
2019-10-27 15:29:50 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:29:50 up  7:09,  1 user,  load average: 0.67, 0.73, 0.87 b1842e5f56a1fd59823f37e1a9471ec9fbb3ab6f

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -34,7 +34,7 @@ make_EHelper(cmp) {
   }
 
   rtl_update_ZFSF(&s1,id_dest->width);
-  rtl_is_add_carry(&s1,&s1,&id_dest->val);
+  rtl_is_sub_carry(&s1,&s1,&id_dest->val);
   rtl_set_CF(&s1);
   rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s0);
