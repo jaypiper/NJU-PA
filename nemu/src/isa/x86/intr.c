a0b848bf5351be12861d538f7b66bd4000c7725c
tracer-ics2019 <tracer@njuics.org>
2019-11-08 18:27:37 +0800

    >  compile 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:27:37 up  1:31,  1 user,  load average: 0.91, 0.70, 0.61 511d66f98c732a9e755a58ab15be1d0d4aa230e

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -6,7 +6,7 @@ void raise_intr(uint32_t NO, vaddr_t ret_addr) {
    */
   rtl_push(&cpu.eflags);
   rtl_push(&cpu.CS);
-  rtl_push(&cpu.pc);
+  rtl_push(&ret_addr);
   rtl_j((vaddr_read(cpu.IDTR._addr + 8*NO +4,4) & 0xFFFF0000) 
       + (vaddr_read(cpu.IDTR._addr + 8*NO, 4) & 0x0000FFFF));
 
