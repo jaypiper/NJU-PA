f96bb4c3e59af570668fd46a0d88d186f626091a
tracer-ics2019 <tracer@njuics.org>
2019-11-08 20:49:10 +0800

    >  run 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  20:49:10 up  3:52,  1 user,  load average: 0.63, 0.48, 0.56 9b5280c2b8921b0721e12d4bdcc49ec0143ddc66

--- nexus-am/am/build/x86-nemu/src/x86/nemu/cte.d
+++ nexus-am/am/build/x86-nemu/src/x86/nemu/cte.d
@@ -2,4 +2,5 @@
  /home/piper/ics2019/nexus-am/am/src/x86/nemu/cte.c \
  /home/piper/ics2019/nexus-am/am/am.h \
  /home/piper/ics2019/nexus-am/am/include/arch/x86-nemu.h \
- /home/piper/ics2019/nexus-am/am/include/x86.h
+ /home/piper/ics2019/nexus-am/am/include/x86.h \
+ /home/piper/ics2019/nexus-am/libs/klib/include/klib.h
