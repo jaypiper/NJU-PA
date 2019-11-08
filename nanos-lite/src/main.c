b4581b2fa4e01b9f11fa9f1eb313f71834d1f6bb
tracer-ics2019 <tracer@njuics.org>
2019-11-08 18:01:45 +0800

    >  run 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  18:01:45 up  1:05,  1 user,  load average: 0.68, 0.63, 0.49 8a632dfd480d5e40456ed6d5f4c38d651a9c78b5

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -23,15 +23,15 @@ int main() {
   init_irq();
 #endif
 
-//   init_fs();
+  // init_fs();
 
-//   init_proc();
+  // init_proc();
 
-//   Log("Finish initialization");
+  // Log("Finish initialization");
 
-// #ifdef HAS_CTE
-//   _yield();
-// #endif
+#ifdef HAS_CTE
+  _yield();
+#endif
 
-//   panic("Should not reach here");
+  panic("Should not reach here");
 }
