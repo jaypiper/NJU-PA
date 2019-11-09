9492a7924804fa6028284f6ab65bdaccdf037e9e
tracer-ics2019 <tracer@njuics.org>
2019-11-09 08:37:39 +0800

    >  run 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:37:39 up 15:41,  1 user,  load average: 0.73, 0.54, 0.55 8c778afce13abac2930f304ca4274043b36b51c

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,13 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case 0: Log("EVENT_NULL"); break;
+    case 1: Log("EVENT_ERROR"); break;
+    case 2: Log("EVENT_IRQ_TIMER"); break;
+    case 3: Log("EVENT_IQR_IODEV"); break;
+    case 4: Log("EVENT_PAGEFAULT"); break;
+    case 5: Log("EVENT_YIELD"); break;
+    case 6: Log("EVENT_SYSCALL"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
