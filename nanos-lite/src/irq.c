5b3863222a0ab7e8bf53de930a79ae128d9e6f80
tracer-ics2019 <tracer@njuics.org>
2019-11-09 09:03:36 +0800

    >  run 181250012 piper Linux piper 5.0.0-29-generic #31-Ubuntu SMP Thu Sep 12 13:05:32 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  09:03:36 up 16:07,  1 user,  load average: 0.41, 0.48, 0.56 8cb9f853ff4cf0a0dc0c8059274bf2c3750038e1

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,8 +3,8 @@
 static _Context* do_event(_Event e, _Context* c) {
   printf("reach!");
   switch (e.event) {
-    case 0: Log("EVENT_NULL"); break;
-    case 1: Log("EVENT_ERROR"); break;
+    case 0: Log("EVENT_NULL\n"); break;
+    case 1: Log("EVENT_ERROR\n"); break;
     case 2: Log("EVENT_IRQ_TIMER"); break;
     case 3: Log("EVENT_IQR_IODEV"); break;
     case 4: Log("EVENT_PAGEFAULT"); break;
