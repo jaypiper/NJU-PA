f85f46cdfd3f1ad4073d17e47c6b01694a0448a0
tracer-ics2019 <tracer@njuics.org>
2019-11-16 22:31:48 +0800

    >  compile 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  22:31:48 up 1 min,  1 user,  load average: 2.12, 0.68, 0.24 485fcc964157166a3da0d5f2c9214879f32c338c

--- nexus-am/tests/amtest/build/x86-nemu/src/main.d
+++
@@ -1,6 +0,0 @@
-/home/piper/ics2019/nexus-am/tests/amtest/build/x86-nemu//./src/main.o: \
- /home/piper/ics2019/nexus-am/tests/amtest/src/main.c \
- /home/piper/ics2019/nexus-am/tests/amtest/include/amtest.h \
- /home/piper/ics2019/nexus-am/am/am.h \
- /home/piper/ics2019/nexus-am/am/include/arch/x86-nemu.h \
- /home/piper/ics2019/nexus-am/libs/klib/include/klib.h
--- nexus-am/tests/amtest/build/x86-nemu/src/tests/intr.d
+++
@@ -1,6 +0,0 @@
-/home/piper/ics2019/nexus-am/tests/amtest/build/x86-nemu//./src/tests/intr.o: \
- /home/piper/ics2019/nexus-am/tests/amtest/src/tests/intr.c \
- /home/piper/ics2019/nexus-am/tests/amtest/include/amtest.h \
- /home/piper/ics2019/nexus-am/am/am.h \
- /home/piper/ics2019/nexus-am/am/include/arch/x86-nemu.h \
- /home/piper/ics2019/nexus-am/libs/klib/include/klib.h
