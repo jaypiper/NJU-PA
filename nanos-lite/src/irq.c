fe9154164a4b6e15fcf11f5ad2705acfb19d39e3
tracer-ics2019 <tracer@njuics.org>
2019-11-22 08:53:02 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:53:02 up  1:34,  1 user,  load average: 0.55, 0.58, 0.50 cf47ce865ed01e594651164a519d3524b0eb9c59

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,13 +4,13 @@ static _Context* do_event(_Event e, _Context* c) {
  // Log("reach");
   //printf("after reach");
   switch (e.event) {
-    case 0: printf("EVENT NULL\n"); break;
-    case 1: printf("EVENT ERROR\n"); break;
-    case 2: printf("EVENT IRQ TIMER\n"); break;
-    case 3: printf("EVENT IQR IODEV\n"); break;
-    case 4: printf("EVENT PAGEFAULT\n"); break;
-    case 5: printf("EVENT YIELD\n"); break;
-    case 6: printf("EVENT SYSCALL\n"); break;
+    // case 0: printf("EVENT NULL\n"); break;
+    // case 1: printf("EVENT ERROR\n"); break;
+    // case 2: printf("EVENT IRQ TIMER\n"); break;
+    // case 3: printf("EVENT IQR IODEV\n"); break;
+    // case 4: printf("EVENT PAGEFAULT\n"); break;
+    // case 5: printf("EVENT YIELD\n"); break;
+    // case 6: printf("EVENT SYSCALL\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
   //printf("the end");
