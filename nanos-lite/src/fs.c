5dbf243256a37c7ece2797bcd08c8d914fd10b46
tracer-ics2019 <tracer@njuics.org>
2019-11-22 21:12:36 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  21:12:36 up 35 min,  1 user,  load average: 0.89, 0.81, 0.60 6f01b0e71af13797c3a600a2ea84e86c3600d6e8

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -9,6 +9,7 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;   //类型是off_t?？   //QAQ对应fireable应该加在最后面
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -28,11 +29,66 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, invalid_read, invalid_write},
   {"stderr", 0, 0, invalid_read, invalid_write},
-#include "files.h"
+#include "files.h"      //这种包括方法也太强了！
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+int fs_open(const char* pathname, int flags, int mode){
+  //通过pathname进行索引，找到的话返回文件描述符，否则assertion(assert?)终止
+  int idx = 0;
+  for(idx = 0; idx < NR_FILES; idx++){
+    if(strcmp(file_table[idx].name, pathname) == 0) break;
+  }
+  assert(idx < NR_FILES);
+  file_table[idx].open_offset = 0;
+  return idx;
+}
+/*尝试从fd(file descroptor)中读取len个字节，放入buf开始的位置，返回读取字节数
+  增加file offset（open offset),（读取字节数, 不一定所有能读到len字节)
+  =W=我不想检测len是否为0
+*/
+size_t fs_read(int fd, void *buf, size_t len){
+  size_t read_size = len;
+  if(file_table[fd].open_offset + len > file_table[fd].size)
+    read_size = file_table[fd].size - file_table[fd].open_offset;
+  read_size = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, read_size);
+  file_table[fd].open_offset += read_size;// 到底是加之前的还是之后的呢？
+  return read_size;
+}
+
+/* 从offset开始写入吗
+*/
+size_t fs_write(int fd, const void *buf, size_t len){
+  size_t write_size = len;
+  if(file_table[fd].open_offset + len > file_table[fd].size)
+    write_size = file_table[fd].size - file_table[fd].open_offset;
+  write_size = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, write_size);
+  return write_size;
+}
+/* 允许open_offset超过文件大小,但讲义上面说不要，因此如果超过那么操作
+   无效，并返回-1*/
+size_t fs_lseek(int fd, size_t offset, int whence){
+  size_t newoff = file_table[fd].open_offset;
+  switch(whence){
+    case SEEK_SET: newoff = offset; break;
+    case SEEK_CUR: newoff += offset; break;
+    case SEEK_END: newoff = file_table[fd].size + offset; break;
+    default: return -1;
+  }
+  if(newoff > file_table[fd].size) return -1;
+  file_table[fd].open_offset = newoff;
+  return newoff;
+}
+
+int fs_close(int fd){
+  file_table[fd].open_offset = 0;
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,35 +11,31 @@
 #endif
 
 #define PBEGIN 0x3000000;
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+int fs_open(const char* pathname, int flags, int mode);
+int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  
   //判断是否为elf：我没写QAQ
+  int idx = fs_open(filename, 0, 0);
   Elf_Ehdr elf;
-  ramdisk_read(&elf, 0, sizeof(Elf_Ehdr));
-  // for(int i = 0; i < sizeof(Elf_Ehdr)/4; i++) printf("%x \n",*((uint32_t*)&elf+i));
-  // printf("\n");
-  for(int i = 0; i < elf.e_phnum; i++){
-   // printf("\n%d done\n", i);
-    Elf_Phdr ent; 
-    //uint32_t type;
-    ramdisk_read(&ent, elf.e_phoff + i * elf.e_phentsize, elf.e_phentsize);
+  fs_read(idx, &elf, sizeof(Elf_Ehdr));
 
-    // for(int i = 0; i < elf.e_phentsize/4; i++) printf("%x \n",*((uint32_t*)&ent+i));
-    // printf("\n");
-    
+  for(int i = 0; i < elf.e_phnum; i++){
+    Elf_Phdr ent; 
+    fs_read(idx, &ent, elf.e_phentsize);
+
     if(ent.p_type == PT_LOAD){
       uint8_t  buf[ent.p_memsz+1]; //需要+1吗
       
-      ramdisk_read(buf, ent.p_offset, ent.p_filesz);
+      fs_read(idx, buf, ent.p_filesz);
       for(int j = ent.p_filesz; j <= ent.p_memsz; j++) buf[j] = 0;
       memcpy((void*)ent.p_vaddr, buf , ent.p_memsz);
      
     }
   }
-
+  fs_close(idx);
   return elf.e_entry;
 }
 
