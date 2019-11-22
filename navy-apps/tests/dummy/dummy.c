44649ce5f93914e0596ea140fcec85880115568b
tracer-ics2019 <tracer@njuics.org>
2019-11-22 08:42:09 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:42:09 up  1:23,  1 user,  load average: 0.50, 0.55, 0.44 ad8c75a847282266de7aa76a49e93f13b8b30e7f

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,10 +1,10 @@
 #include <stdint.h>
-#include<stdio.h>
+#include<assert.h>
 
 #define SYS_yield 1
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  printf("dummy\n");
+  assert(0);
   return _syscall_(SYS_yield, 0, 0, 0);
 }
