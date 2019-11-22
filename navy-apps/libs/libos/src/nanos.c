1d375dbe708888e9197cb5d8d67a6d297b54de70
tracer-ics2019 <tracer@njuics.org>
2019-11-22 08:36:36 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:36:36 up  1:18,  1 user,  load average: 0.26, 0.48, 0.38 600b595ce4a8f935a3964991d83f2986c79ae0f6

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -49,9 +49,9 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 }
 
 void _exit(int status) {
-  
+  assert(0);
   _syscall_(SYS_exit, status, 0, 0);
-  while (1);
+  //while (1);
 }
 
 int _open(const char *path, int flags, mode_t mode) {
