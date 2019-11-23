71cd58f9f31dfcd042d90a106211dd634812d4f8
tracer-ics2019 <tracer@njuics.org>
2019-11-23 13:27:29 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:27:29 up  5:56,  1 user,  load average: 0.62, 0.61, 0.52 2453a6b7c51599bd7ace610e7142e1368258066

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,6 +27,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  printf("len: %d, offset: %d", len, offset);
   const char* disp = buf;
   len /= 4;
   for(int i = 0; i < len/4; i++){
