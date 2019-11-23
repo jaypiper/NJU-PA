559c819edf8d9a8fa0f8f0aeb08e7d4b3a38d71e
tracer-ics2019 <tracer@njuics.org>
2019-11-23 15:43:18 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:43:18 up  8:12,  1 user,  load average: 0.56, 0.46, 0.42 669257f034dcc7ea798f6afa4e897e92ab436268

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -64,11 +64,11 @@ int _write(int fd, void *buf, size_t count) {
 extern char _end;
 
 void *_sbrk(intptr_t increment) {
-  static void* p_break = &_end;   //原来end只是存在最后的符号而不是指示最后的指针
-  void* old_break = p_break;    //本来想卸载syscal里面，但返回值不好传
+  static char* p_break = &_end;   //原来end只是存在最后的符号而不是指示最后的指针
+  char* old_break = p_break;    //本来想卸载syscal里面，但返回值不好传
   p_break += increment;
   _syscall_(SYS_brk, (intptr_t)p_break, 0, 0);
-  return (void*)old_break;   //后面还需不需要改呢？
+  return (void)old_break;   //后面还需不需要改呢？
   //return (void *)-1;
 }
 
