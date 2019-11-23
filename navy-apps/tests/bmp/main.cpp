913cc16fb68d4187565b800a6fb3aa5ff8bafb80
tracer-ics2019 <tracer@njuics.org>
2019-11-23 15:54:47 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:54:47 up  8:23,  1 user,  load average: 0.93, 0.76, 0.61 33b17cfa3b745726540027a273dddc191cb15f10

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -7,11 +7,17 @@ int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
   printf("pass1\n");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  // assert(bmp->pixels);
-  // NDL_OpenDisplay(bmp->w, bmp->h);
-  // NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  // NDL_Render();
-  // NDL_CloseDisplay();
-  // while (1);
+  printf("pass2\n");  
+  assert(bmp->pixels);
+  printf("pass3\n");
+  
+  NDL_OpenDisplay(bmp->w, bmp->h);
+  printf("pass4\n");
+  NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
+  printf("pass5\n");
+  NDL_Render();
+  printf("pass6\n");
+  NDL_CloseDisplay();
+  while (1);
   return 0;
 }
