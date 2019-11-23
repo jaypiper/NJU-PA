209d8b1743a28aa82c41237e89987a5c1778cc93
tracer-ics2019 <tracer@njuics.org>
2019-11-23 13:38:28 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:38:28 up  6:07,  1 user,  load average: 0.50, 0.63, 0.61 c7a8b59c8e5445aa62740d8e8eec55f8c1af5c6

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,12 +22,13 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  if(len + offset > sizeof(dispinfo)) len = sizeof(dispinfo) - offset;
   memcpy(buf, dispinfo+offset, len);
-  return 0;
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  //printf("len: %d, offset: %d", len, offset);
+  printf("len: %d, offset: %d", len, offset);
   const char* disp = buf;
   len /= 4;
   for(int i = 0; i < len/4; i++){
@@ -48,7 +49,7 @@ void init_device() {
   Log("Initializing devices...");
   _ioe_init();
   sprintf(dispinfo, "WIDTH: %d\nHEIGHT: %d\n", screen_width(), screen_height());
-  printf("%s\n",dispinfo);
+  //printf("%s\n",dispinfo);
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
