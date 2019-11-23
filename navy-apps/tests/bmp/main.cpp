760215673f914337d46b7245ff217a78323e50d1
tracer-ics2019 <tracer@njuics.org>
2019-11-23 14:29:58 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:29:58 up  6:58,  1 user,  load average: 1.04, 0.69, 0.59 5b035dec9b02d949a7943585b0fc8d25d8716935

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -1,22 +1,14 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <ndl.h>
-#include <stdio.h>
 
 int main() {
-  printf("reach-0");
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
-  printf("reach-1");
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  printf("reach-2");
   assert(bmp->pixels);
-  printf("reach-3");
   NDL_OpenDisplay(bmp->w, bmp->h);
-  printf("reach-4");
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  printf("reach-5");
   NDL_Render();
-  printf("reach-6");
   NDL_CloseDisplay();
   while (1);
   return 0;
