5c8a361840056516fef70892eec4c089e946c31f
tracer-ics2019 <tracer@njuics.org>
2019-11-23 15:56:35 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  15:56:35 up  8:25,  1 user,  load average: 1.06, 0.86, 0.66 1a5ea576b0621f103fde20b3da9e1f331639adea

--- navy-apps/libs/libndl/src/bmp.c
+++ navy-apps/libs/libndl/src/bmp.c
@@ -28,8 +28,8 @@ int NDL_LoadBitmap(NDL_Bitmap *bmp, const char *filename) {
   if (!(fp = fopen(filename, "r"))) return -1;
 
   struct BitmapHeader hdr;
-  // assert(sizeof(hdr) == 54);
-  // assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
+  assert(sizeof(hdr) == 54);
+  //assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
 
   if (hdr.bitcount != 24) return -1;
   if (hdr.compression != 0) return -1;
