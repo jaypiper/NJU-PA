6d532f9bd79d8e8b8815c45a837b1b7c1274e3b0
tracer-ics2019 <tracer@njuics.org>
2019-11-23 08:22:16 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:22:16 up 51 min,  1 user,  load average: 1.14, 0.72, 0.54 fa32ef80842497ff94ad8852e7194ad9577bc4

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,11 @@
 #include "common.h"
 #include "syscall.h"
 
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+int fs_open(const char* pathname, int flags, int mode);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -12,23 +17,31 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_exit: _halt(a[2]); break;
     case SYS_yield: _yield(); break;
-    case SYS_write: 
-                  if(a[3] == 0 || (a[1] != 1 && a[1] != 2)) {
+    case SYS_write: if(a[3] == 0 || (a[1] != 1 && a[1] != 2)) {
                     c->GPRx = 0;  //x是返回值吗呜呜呜
                     break;
-                  }
-                  c->GPRx = 1;
-                  char* out_ = (char*)a[2];
-                  for(int i = 0; i  < a[3]; i++) {
-                    _putc(*out_);
-                    out_++;
-                  }
-                  break;
+                    }
+                    c -> GPRx =fs_write(a[1],(void*)a[2], a[3]);
+                    break;
     case SYS_brk: c  -> GPRx = 0; break;
+    case SYS_read: c -> GPRx = fs_read(a[1],(void*)a[2], a[3]); break;
+    case SYS_close: fs_close(a[1]); break;
+    case SYS_lseek: c -> GPRx = fs_lseek(a[1], a[2], a[3]); break;
+    case SYS_open: c -> GPRx = fs_open((char*)a[1], a[2], a[3]); break;
                   
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-
+// if(a[3] == 0 || (a[1] != 1 && a[1] != 2)) {
+//                     c->GPRx = 0;  //x是返回值吗呜呜呜
+//                     break;
+//                   }
+//                   c->GPRx = 1;
+//                   char* out_ = (char*)a[2];
+//                   for(int i = 0; i  < a[3]; i++) {
+//                     _putc(*out_);
+//                     out_++;
+//                   }
+//                   break;
   return NULL;
 }
