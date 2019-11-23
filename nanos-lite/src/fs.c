6e19e94b7be999018f4deed856f87c425023188b
tracer-ics2019 <tracer@njuics.org>
2019-11-23 09:05:47 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  09:05:47 up  1:34,  1 user,  load average: 0.65, 0.45, 0.38 b681e29d1ec0382a6222884bb94c09ac0d79e25

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,7 +57,7 @@ int fs_open(const char* pathname, int flags, int mode){
   =W=我不想检测len是否为0
 */
 size_t fs_read(int fd, void *buf, size_t len){
-  printf("read fd: %d\n",fd);
+  //printf("read fd: %d\n",fd);
   if(fd < 3) return 0;
   size_t read_size = len;
   if(file_table[fd].open_offset + len > file_table[fd].size)
@@ -74,14 +74,14 @@ size_t fs_write(int fd, const void *buf, size_t len){
   if(file_table[fd].open_offset + len > file_table[fd].size)
     write_size = file_table[fd].size - file_table[fd].open_offset;
   //printf("fd: %d\n", fd);
-  if(fd == 1 || fd == 2) {
+ // if(fd == 1 || fd == 2) {
     printf("%s",buf);                       //这样实现真的okkk？
     // char* out_ = (char*)buf;
     // for(int i = 0; i  < write_size; i++) {
     //   _putc(*out_);
     //   out_++;
     // }
-  }
+  //}
   // else 
   // ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, write_size);
   return write_size;
