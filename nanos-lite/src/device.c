d1405218fe148cff9c3482b25e177da00762072c
tracer-ics2019 <tracer@njuics.org>
2019-11-23 17:52:37 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:52:37 up 10:21,  1 user,  load average: 0.66, 0.73, 0.65 4f23d2e6bf67b3597424b1c0c437c529858022b9

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,26 +28,26 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  printf("len: %d, offset: %d", len, offset);
-  len /= 4; offset /= 4;
-  int x = offset % screen_width();
-  int y = offset / screen_width();
+  // printf("len: %d, offset: %d", len, offset);
+  // len /= 4; offset /= 4;
+  // int x = offset % screen_width();
+  // int y = offset / screen_width();
   
-  for(int beg = 0; beg < len; ){
-    if(len - beg > screen_width() - x){
-    //  draw_rect((uint32_t*) buf, x, y, screen_width()-x, 1);
-      beg += screen_width() - x;
-      x = 0; y++;
-    }
-    else{
-   //   draw_rect((uint32_t*)buf, x, y, len - beg, 1);
-      x += len - beg;
-      beg = len;
-    }
-    // draw_rect((uint32_t*)disp, x, y, 1, 1);
-    // offset ++;
-    // disp += 4;
-  }
+  // for(int beg = 0; beg < len; ){
+  //   if(len - beg > screen_width() - x){
+  //     draw_rect((uint32_t*) buf, x, y, screen_width()-x, 1);
+  //     beg += screen_width() - x;
+  //     x = 0; y++;
+  //   }
+  //   else{
+  //     draw_rect((uint32_t*)buf, x, y, len - beg, 1);
+  //     x += len - beg;
+  //     beg = len;
+  //   }
+  //   // draw_rect((uint32_t*)disp, x, y, 1, 1);
+  //   // offset ++;
+  //   // disp += 4;
+  // }
   return 0;
 }
 
