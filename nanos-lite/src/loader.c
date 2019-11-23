62961da351f4ef6746fcb89e37a8884ca0b14793
tracer-ics2019 <tracer@njuics.org>
2019-11-23 17:12:49 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:12:49 up  9:41,  1 user,  load average: 0.49, 0.53, 0.58 65ab9600f9b33d1b86c561b9561dab9289903085

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -46,8 +46,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  printf("Jump to entry = %x", entry);
-  printf("\nentry: %d %x\n", entry, entry);
+  Log("Jump to entry = %x", entry);
+  //printf("\nentry: %d %x\n", entry, entry);
 
   ((void(*)())entry) ();
 }
