c21eed28655652b48135d3dd0fd84c707feb3667
tracer-ics2019 <tracer@njuics.org>
2019-11-24 08:26:17 +0800

    >  run 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  08:26:17 up 45 min,  1 user,  load average: 0.26, 0.12, 0.13 92115b5af330381b69794586b1b39dd634b0dde2

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -5,20 +5,20 @@
 
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
-  printf("pass1\n");
-  NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  printf("pass2\n");  
-  assert(bmp->pixels);
-  printf("pass3\n");
-  printf("%d, %d\n", bmp->w, bmp->h);
+  // printf("pass1\n");
+  // NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
+  // printf("pass2\n");  
+  // assert(bmp->pixels);
+  // printf("pass3\n");
+  // printf("%d, %d\n", bmp->w, bmp->h);
   
-  NDL_OpenDisplay(bmp->w, bmp->h);
-  printf("pass4\n");
-  NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  printf("pass5\n");
-  NDL_Render();
-  printf("pass6\n");
-  NDL_CloseDisplay();
-  while (1);
+  // NDL_OpenDisplay(bmp->w, bmp->h);
+  // printf("pass4\n");
+  // NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
+  // printf("pass5\n");
+  // NDL_Render();
+  // printf("pass6\n");
+  // NDL_CloseDisplay();
+  // while (1);
   return 0;
 }
