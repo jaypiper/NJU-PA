c6f6c0d39a9896595c73fcb111699634aa72f4ee
tracer-ics2019 <tracer@njuics.org>
2019-11-30 16:29:36 +0800

    >  compile 181250012 piper Linux piper 5.0.0-32-generic #34-Ubuntu SMP Wed Oct 2 02:06:48 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  16:29:36 up  1:02,  1 user,  load average: 0.88, 0.60, 0.63 40fe63b9a63de73721aec7ce2ddb47723743f487

--- nemu/src/isa/x86/exec/data-mov.c
+++ nemu/src/isa/x86/exec/data-mov.c
@@ -87,9 +87,9 @@ make_EHelper(movzx) {
 }
 
 make_EHelper(movs){
-    
-     s0 = *((uint8_t*)(intptr_t)cpu.esi);
-     *((uint8_t*)(intptr_t)cpu.edi) = (uint8_t)s0;
+    cpu.edi = cpu.esi;
+    //  s0 = *((uint8_t*)(intptr_t)cpu.esi);
+    //  *((uint8_t*)(intptr_t)cpu.edi) = (uint8_t)s0;
     // rtl_mv(cpu.edi, &s0);
     // rtl_mv(&s1,&cpu.edi);
     
